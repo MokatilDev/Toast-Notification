4dd4d6f7d7ccc71dd40f7839d48d488e08ed7336
MokatilDev <mokatilgfx@gmail.com>
2023-07-17 14:38:34 +0100

    Add event click on button for show toats

---
+++ script.js
@@ -0,0 +1,50 @@
+const Notifications = document.querySelector("ul");
+const Success = document.querySelector("li");
+const btns = document.querySelectorAll("button");
+
+
+const details = {
+    Success: {
+        icon: "fa-solid fa-circle-check",
+        text:"Success: This is a success toast",
+    },
+    Error: {
+        icon: "fa-solid fa-circle-xmark",
+        text:"Error: This is an error toast",
+    },
+    Warning: {
+        icon: "fa-solid fa-circle-exclamation",
+        text:"Warning: This is a warning toast",
+    },
+    Info: {
+        icon: "fa-solid fa-solid fa-circle-info",
+        text:"Info: This is an infomartion toast",
+    },
+}
+
+function toats(e){
+    return `<div>
+    <i class="fa-sharp ${details[e].icon}"></i>
+    <span>${details[e].text}</span>
+    </div>
+    <i class="fa-sharp fa-solid fa-xmark"></i>`
+}
+
+
+
+btns.forEach(element =>{
+    element.addEventListener("click", () => {
+        const li = document.createElement("li")
+        li.setAttribute("id",`${element.id}Toast`)
+        li.innerHTML = toats(element.id)
+        console.log(element.id);
+        Notifications.appendChild(li)
+        setTimeout(()=>{
+            li.remove()
+        },5000)
+    });
+})
+
+
+
+
