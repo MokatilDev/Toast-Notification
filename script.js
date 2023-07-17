92d89e256975c9bddb77409688030a9e2a22cd96
MokatilDev <mokatilgfx@gmail.com>
2023-07-17 15:07:29 +0100

    Add Close event

--- script.js
+++ script.js
@@ -6,42 +6,50 @@ const btns = document.querySelectorAll("button");
 const details = {
     Success: {
         icon: "fa-solid fa-circle-check",
-        text:"Success: This is a success toast",
+        text: "Success: This is a success toast",
     },
     Error: {
         icon: "fa-solid fa-circle-xmark",
-        text:"Error: This is an error toast",
+        text: "Error: This is an error toast",
     },
     Warning: {
         icon: "fa-solid fa-circle-exclamation",
-        text:"Warning: This is a warning toast",
+        text: "Warning: This is a warning toast",
     },
     Info: {
         icon: "fa-solid fa-solid fa-circle-info",
-        text:"Info: This is an infomartion toast",
+        text: "Info: This is an infomartion toast",
     },
 }
 
-function toats(e){
+function Remove1(e) {
+    e.classList.add("hide")
+    setTimeout(() => {
+        e.remove()
+    }, 290)
+}
+
+function toats(e) {
     return `<div>
     <i class="fa-sharp ${details[e].icon}"></i>
     <span>${details[e].text}</span>
     </div>
-    <i class="fa-sharp fa-solid fa-xmark"></i>`
+    <i onclick="Remove1(this.parentElement)" class="fa-sharp fa-solid fa-xmark"></i>`
 }
 
 
-
-btns.forEach(element =>{
+btns.forEach(element => {
     element.addEventListener("click", () => {
         const li = document.createElement("li")
-        li.setAttribute("id",`${element.id}Toast`)
+        li.setAttribute("id", `${element.id}Toast`)
         li.innerHTML = toats(element.id)
-        console.log(element.id);
         Notifications.appendChild(li)
-        setTimeout(()=>{
-            li.remove()
-        },5000)
+        setTimeout(() => {
+            li.classList.add("hide")
+            setTimeout(() => {
+                li.remove()
+            }, 290)
+        }, 5000)
     });
 })
 
